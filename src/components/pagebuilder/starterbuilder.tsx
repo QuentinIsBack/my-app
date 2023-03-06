216e7021dba7e09df09481156f0a1442d25bbd51
pro.quentint@gmail.com <ImQuentinT>
2023-03-06 16:50:30 +0100

    commit

--- src/components/pagebuilder/starterbuilder.tsx
+++ src/components/pagebuilder/starterbuilder.tsx
@@ -1,19 +1,32 @@
 import { useTitle } from "../../hooks/useTitle"
 
+
+interface SBthemes {
+    default: string,
+    pages: string,
+}
+export const SBthemes = {
+    home: "bg-[url('https://images.pexels.com/photos/5825604/pexels-photo-5825604.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')] bg-cover bg-center bg-black",
+    pages: "bg-gradient-to-b from-indigo-600 to-pink-500"
+};
+
 type PageType = {
     show: boolean | false,
-
+    theme: string,
     children: JSX.Element
+    footer: boolean
 }
 
 export const StarterBuilder = ({
     children,
+    theme,
+    footer,
     show,
 }: PageType) => {
     return (
         <>
             <div className="grid grid-cols-2 h-full w-full">
-                <div className="bg-gradient-to-b from-indigo-600 to-pink-500">
+                <div className={`${theme}`}>
 
                 </div>
                 <div className="relative flex flex-col">
@@ -21,11 +34,11 @@ export const StarterBuilder = ({
                         
                     </div>
                     <div className="grow">
-                    {children}
+                        {children}
                     </div>
-                    <div className="h-[var(--footer--starter)] border-t">
+                    {footer && <div className="h-[var(--footer--starter)] border-t">
                         
-                    </div>
+                    </div>}
                 </div>
             </div>
         </>
@@ -33,5 +46,7 @@ export const StarterBuilder = ({
 }
 
 StarterBuilder.defaultProps = {
-    show: true
+    show: true,
+    footer: false,
+    theme: SBthemes.pages
 }
--- src/pages/starter/overview.tsx
+++ src/pages/starter/overview.tsx
@@ -1,28 +1,30 @@
 import { PageBuilder } from "../../components/pagebuilder/pagebuilder";
 
 import { useNavigate } from "react-router-dom";
-import { StarterBuilder } from "../../components/pagebuilder/starterbuilder";
+import { SBthemes, StarterBuilder } from "../../components/pagebuilder/starterbuilder";
 import { useContext } from "react";
 import { AgencyContext } from "../../contexts/AgencyContext";
 import Icon from "../../components/icon/icons";
+import { UserContext } from "../../contexts/UserContext";
 
 function App() {
 
     const { AgencyData } = useContext(AgencyContext)
+    const { UserData } = useContext(UserContext)
+
     const navigate = useNavigate();
 
     return (
         <PageBuilder title="Starter" show={true}>
             <>
-                <StarterBuilder>
+                <StarterBuilder theme={SBthemes.home}>
                     <>
                         <div className="flex flex-col justify-center items-center w-full h-full">
                             <div className="w-35rem max-w-35rem overflow-hidden">
 
                                 <div className="flex flex-col space-y-10">
                                     <div>
-                                        <div className="text-xl font-semibold text-supergray">Commencer une nouvelle annonce</div>
-
+                                        <div className="text-3xl font-semibold text-supergray">Bon retour, {UserData.getFirstname()}</div>
                                     </div>
 
                                     <div>
@@ -46,6 +48,20 @@ function App() {
                                             </>
                                         }
                                     </div>
+
+                                    <div>
+                                        <div className="text-xl font-semibold text-supergray">Commencer une nouvelle annonce</div>
+                                        <button className="h-6rem flex flex-row justify-between items-center border-b w-full mt-6">
+                                            <div className="flex flew-row items-center space-x-4">
+                                                <div><Icon size={35} name="IoHomeOutline" /></div>
+                                                <div className="text-base font-medium text-supergray">Créer une nouvelle annonce</div>
+                                            </div>
+                                            <div>
+                                                <Icon size={20} name="IoChevronForward" />
+                                            </div>
+                                        </button>
+                                    </div>
+
                                 </div>
 
                             </div>
